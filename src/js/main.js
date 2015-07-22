6162a806f532ccc50bdc7d3ee46ddcb76a207260
Kirk Morris <kirk@saricden.com>
2015-07-21 23:05:06 -0400

    More homepage responsiveness.

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,31 @@
+var mobileMenu = document.querySelector('.mobile-menu');
+var homeNav = document.querySelector('nav.homepage_var');
+var navClose = document.querySelector('.navclose');
+var floatNav = document.querySelector('.floating_nav');
+
 var fadeNavbar = function(e) {
   var doc = document.documentElement;
   var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
   if (top != 0) {
-    document.querySelector('.floating_nav').className = "floating_nav scrolled";
+    floatNav.className = "floating_nav scrolled";
   }
   else {
-    document.querySelector('.floating_nav').className = "floating_nav";
+    floatNav.className = "floating_nav";
   }
 };
 
+var openMobileNav = function(e) {
+  e.preventDefault();
+  mobileMenu.className = "mobile-menu open";
+  homeNav.className = "homepage_var open";
+};
+
+var closeMobileNav = function(e) {
+  e.preventDefault();
+  mobileMenu.className = "mobile-menu";
+  homeNav.className = "homepage_var";
+}
+
 window.addEventListener("scroll", fadeNavbar);
+mobileMenu.addEventListener("click", openMobileNav);
+navClose.addEventListener("click", closeMobileNav);
